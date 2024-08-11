a2345e3da25a333048ab5fe1da1b53d1fb442461
AvidCoder27 <gilireeves@gmail.com>
2024-08-11 13:47:55 -0400

    add more conversions, change seperator from \ to :

--- src/main.rs
+++ src/main.rs
@@ -14,7 +14,7 @@ fn main() {
 
     loop {
         let line = read_input("\nEnter your conversion");
-        if line == String::from("quit\\") {
+        if line == String::from("quit:") {
             break;
         }
 
@@ -67,46 +67,106 @@ fn main() {
 }
 
 fn manually_create_units(generator: &mut IDGenerator, aliases: &mut HashMap<String, usize>, unit_ids: &mut HashMap<usize, Unit>) {
-    let mut yards = Unit::new("yards", generator);
-    let mut feet = Unit::new("feet", generator);
-    let mut centimeters = Unit::new("centimeters", generator);
-    let mut meters = Unit::new("meters", generator);
-    let mut kilometers = Unit::new("kilometers", generator);
-    let mut inches = Unit::new("inches", generator);
+    let mut yard = Unit::new("yards", generator);
+    let mut foot = Unit::new("feet", generator);
+    let mut centimeter = Unit::new("centimeters", generator);
+    let mut meter = Unit::new("meters", generator);
+    let mut kilometer = Unit::new("kilometers", generator);
+    let mut inch = Unit::new("inches", generator);
+    let mut mile = Unit::new("miles", generator);
+    let mut second = Unit::new("seconds", generator);
+    let mut millisecond = Unit::new("milliseconds", generator);
+    let mut minute = Unit::new("minutes", generator);
+    let mut hour = Unit::new("hours", generator);
+    let mut day = Unit::new("days", generator);
+    let mut week = Unit::new("weeks", generator);
+    let mut calendar_year = Unit::new("calendar years", generator);
 
-    insert_aliases(aliases, &yards, vec!["yds", "yards", "yard", "yd"]);
-    insert_aliases(aliases, &feet, vec!["foot", "feet", "ft"]);
-    insert_aliases(aliases, &meters, vec!["m", "meters", "meter"]);
-    insert_aliases(aliases, &kilometers, vec!["km", "kilometers", "kilometer"]);
-    insert_aliases(aliases, &inches, vec!["in", "inch", "inches"]);
-    insert_aliases(aliases, &centimeters, vec!["cm", "centimeter"]);
+    insert_aliases(aliases, &calendar_year, vec!["calendar years", "calendar year"]);
+    insert_aliases(aliases, &week, vec!["week", "weeks"]);
+    insert_aliases(aliases, &day, vec!["days", "day"]);
+    insert_aliases(aliases, &hour, vec!["hours", "hour", "hr", "hrs"]);
+    insert_aliases(aliases, &minute, vec!["minute", "minutes", "min", "mins"]);
+    insert_aliases(aliases, &millisecond, vec!["ms", "millisecond", "milliseconds"]);
+    insert_aliases(aliases, &yard, vec!["yds", "yards", "yard", "yd"]);
+    insert_aliases(aliases, &foot, vec!["foot", "feet", "ft"]);
+    insert_aliases(aliases, &meter, vec!["m", "meters", "meter"]);
+    insert_aliases(aliases, &kilometer, vec!["km", "kilometers", "kilometer"]);
+    insert_aliases(aliases, &inch, vec!["in", "inch", "inches"]);
+    insert_aliases(aliases, &centimeter, vec!["cm", "centimeter"]);
+    insert_aliases(aliases, &mile, vec!["mi", "miles", "mile"]);
+    insert_aliases(aliases, &second, vec!["seconds", "s", "sec", "secs", "second"]);
+
+    let ms_to_seconds = Conversion::new(1.0, 1000.0);
+    second.push_edge(&millisecond, ms_to_seconds.inverse());
+    millisecond.push_edge(&second, ms_to_seconds);    
+
+    let second_to_min = Conversion::new(1.0, 60.0);
+    let minute_to_hour = second_to_min.clone();
+
+    minute.push_edge(&second, second_to_min.inverse());
+    second.push_edge(&minute, second_to_min);
+    hour.push_edge(&minute, minute_to_hour.inverse());
+    minute.push_edge(&hour, minute_to_hour);
+
+    let hour_to_day = Conversion::new(1.0, 24.0);
+    day.push_edge(&hour, hour_to_day.inverse());
+    hour.push_edge(&day, hour_to_day);
+
+    let day_to_week = Conversion::new(1.0, 7.0);
+    week.push_edge(&day, day_to_week.inverse());
+    day.push_edge(&week, day_to_week);
+
+    let day_to_calendar_year = Conversion::new(1.0, 365.0);
+    calendar_year.push_edge(&day, day_to_calendar_year.inverse());
+    day.push_edge(&calendar_year, day_to_calendar_year);
 
     let yards_to_feet = Conversion::new(3.0, 1.0);
-    feet.push_edge(&yards, yards_to_feet.inverse());
-    yards.push_edge(&feet, yards_to_feet);
+    foot.push_edge(&yard, yards_to_feet.inverse());
+    yard.push_edge(&foot, yards_to_feet);
 
     let feet_to_meters = Conversion::new(1.0, 3.28084);
-    meters.push_edge(&feet, feet_to_meters.inverse());
-    feet.push_edge(&meters, feet_to_meters);
+    meter.push_edge(&foot, feet_to_meters.inverse());
+    foot.push_edge(&meter, feet_to_meters);
 
     let km_to_meters = Conversion::new(1000.0, 1.0);
-    meters.push_edge(&kilometers, km_to_meters.inverse());
-    kilometers.push_edge(&meters, km_to_meters);
+    meter.push_edge(&kilometer, km_to_meters.inverse());
+    kilometer.push_edge(&meter, km_to_meters);
 
     let inches_to_feet = Conversion::new(1.0, 12.0);
-    feet.push_edge(&inches, inches_to_feet.inverse());
-    inches.push_edge(&feet, inches_to_feet);
+    foot.push_edge(&inch, inches_to_feet.inverse());
+    inch.push_edge(&foot, inches_to_feet);
 
     let meters_to_centimeters = Conversion::new(100.0, 1.0);
-    centimeters.push_edge(&meters, meters_to_centimeters.inverse());
-    meters.push_edge(&centimeters, meters_to_centimeters);
+    centimeter.push_edge(&meter, meters_to_centimeters.inverse());
+    meter.push_edge(&centimeter, meters_to_centimeters);
 
-    yards.insert_into(unit_ids);
-    feet.insert_into(unit_ids);
-    meters.insert_into(unit_ids);
-    kilometers.insert_into(unit_ids);
-    inches.insert_into(unit_ids);
-    centimeters.insert_into(unit_ids);
+    let feet_to_miles = Conversion::new(1.0, 5280.0);
+    mile.push_edge(&foot, feet_to_miles.inverse());
+    foot.push_edge(&mile, feet_to_miles);
+
+    let inch_to_cm = Conversion::new(2.54, 1.0);
+    centimeter.push_edge(&inch, inch_to_cm.inverse());
+    inch.push_edge(&centimeter, inch_to_cm);
+
+    let mile_to_km = Conversion::new(1.60934, 1.0);
+    kilometer.push_edge(&mile, mile_to_km.inverse());
+    mile.push_edge(&kilometer, mile_to_km);
+
+    yard.insert_into(unit_ids);
+    foot.insert_into(unit_ids);
+    meter.insert_into(unit_ids);
+    kilometer.insert_into(unit_ids);
+    inch.insert_into(unit_ids);
+    centimeter.insert_into(unit_ids);
+    mile.insert_into(unit_ids);
+    second.insert_into(unit_ids);
+    millisecond.insert_into(unit_ids);
+    minute.insert_into(unit_ids);
+    hour.insert_into(unit_ids);
+    day.insert_into(unit_ids);
+    week.insert_into(unit_ids);
+    calendar_year.insert_into(unit_ids);
 }
 
 fn print_steps(initial_value: f64, starting_unit: &Unit, steps: Vec<Step>, answer: f64, final_unit: &Unit, unit_ids: &HashMap<usize, Unit>) {
@@ -212,7 +272,7 @@ fn extract_unit(line: &str) -> Option<(String, usize)> {
     let mut size: usize = 0;
     for c in line.chars() {
         size += 1;
-        if c == '\\' {
+        if c == ':' {
             return Some((unit.trim().to_string(), size));
         } else {
             unit.push(c);
@@ -230,6 +290,6 @@ fn read_input(prompt: &str) -> String {
         Err(_) => {}
     }
     input = input.trim().to_string();
-    input.push('\\');
+    input.push(':');
     input
 }
